71414cf51b52bc157f625e904c040be1803327ba
James Moore <69149348+jkwmoore@users.noreply.github.com>
2023-03-06 01:32:34 +0000

    Update bash-setup.sh
    Add warning logic to updates.

--- bash-setup.sh
+++ bash-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone the repository to the user's home directory first.
-cd ~/bashrc-jmoore && git pull > /dev/null 2>&1
+cd ~/bashrc-jmoore && git pull > /dev/null 2>&1 && echo -e "Custom bashrc updated from Github." || echo -e "Custom bashrc git update failed."
 
 if [ $? -eq 0 ]; then
 
