00202ecc40dcf6863ed87bee54fdc8fc3d90762e
James Moore <69149348+jkwmoore@users.noreply.github.com>
2023-11-25 15:35:44 +0000

    Update bash-setup.sh
    Add check in the script, not just outside before invoked.

--- bash-setup.sh
+++ bash-setup.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-# Clone the repository to the user's home directory first.
-cd ~/bashrc-jmoore && git pull > /dev/null 2>&1 && 
-echo -e "##################################\nCustom bashrc updated from Github.\n##################################" || 
-echo -e "################################\nCustom bashrc git update failed.\n################################"
-           
-if [ $? -eq 0 ]; then
+if [[ $- == *i* ]]; then
+  # Clone the repository to the user's home directory first.
+  cd ~/bashrc-jmoore && git pull > /dev/null 2>&1 && 
+  echo -e "##################################\nCustom bashrc updated from Github.\n##################################" || 
+  echo -e "################################\nCustom bashrc git update failed.\n################################"
+            
+  if [ $? -eq 0 ]; then
 
-  # Check if the lines are already in the .bashrc file
-  if ! grep -Fxq ". ~/bashrc-jmoore/bashrc" ~/.bashrc
-  then
-    # If not, add them to the .bashrc file
-    echo -e "\n# Load custom bashrc\n. ~/bashrc-jmoore/bashrc" >> ~/.bashrc
-  fi
+    # Check if the lines are already in the .bashrc file
+    if ! grep -Fxq ". ~/bashrc-jmoore/bashrc" ~/.bashrc
+    then
+      # If not, add them to the .bashrc file
+      echo -e "\n# Load custom bashrc\n. ~/bashrc-jmoore/bashrc" >> ~/.bashrc
+    fi
 
-  if ! grep -Fxq ". ~/bashrc-jmoore/bash_aliases" ~/.bashrc
-  then
-    # If not, add them to the .bashrc file
-    echo -e "\n# Load custom bash aliases\n. ~/bashrc-jmoore/bash_aliases" >> ~/.bashrc
-  fi
+    if ! grep -Fxq ". ~/bashrc-jmoore/bash_aliases" ~/.bashrc
+    then
+      # If not, add them to the .bashrc file
+      echo -e "\n# Load custom bash aliases\n. ~/bashrc-jmoore/bash_aliases" >> ~/.bashrc
+    fi
 
-  if ! grep -Fq "~/bashrc-jmoore/bash-setup.sh" ~/.bashrc
-  then
-    # If not, add them to the .bashrc file
-    echo -e "\n# Before enabling, check if git pull + SSH agent forwarding causes high CPU." >> ~/.bashrc
-    echo -e "# Load custom bashrc setup script\n#if [[ \$- == *i* ]]; then\n#    ~/bashrc-jmoore/bash-setup.sh \n#fi" >> ~/.bashrc
-  fi
+    if ! grep -Fq "~/bashrc-jmoore/bash-setup.sh" ~/.bashrc
+    then
+      # If not, add them to the .bashrc file
+      echo -e "\n# Before enabling, check if git pull + SSH agent forwarding causes high CPU." >> ~/.bashrc
+      echo -e "# Load custom bashrc setup script\n#if [[ \$- == *i* ]]; then\n#    ~/bashrc-jmoore/bash-setup.sh \n#fi" >> ~/.bashrc
+    fi
 
-else
-  echo "Error: Git clone failed, setup or update aborted."
+  else
+    echo "Error: Git clone failed, setup or update aborted."
+  fi 
 fi
+
+
